0b39ce4f9a7fc2eee1107ab962db7e5ac1812854
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-13 21:36:28 +0530

    indentation

--- ai.js
+++ ai.js
@@ -4,7 +4,7 @@ import config from './config.json' assert { type: 'json' };
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Ignore any external link recommendations Also give a header of <h2> of the dish before listing the ingredients. do not bold anything in the list of ingredients`
- 
+
 const hf = new HfInference(config.HF_ACCESS_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
