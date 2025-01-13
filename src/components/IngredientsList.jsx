90b180987526a6cb639428c88e7df2fc31de442b
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-13 18:12:40 +0530

    organising code into components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,20 @@
+export default function IngredientsListComponent(props){
+    
+    const ingredientsList = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{(ingredient)}</li>
+    ))
+    
+    return (
+        <section className="ingredientsSection">
+                <span className="ingredientsHeader">Ingredients on hand:</span>
+                <ul>{ingredientsList}</ul>
+                {props.ingredients.length > 3 && <div className="getRecipeContainer">
+                <div className="getRecipeTextContainer">
+                    <span className="getRecipeHeader">Ready for a recipe?</span>
+                    <p className="getRecipeText">Generate a recipe from your list of ingredients</p>
+                </div>
+                <button className="recipeButton" onClick={props.recipeToggle}>Get a recipe</button>
+                </div>}
+        </section>
+    )
+}
